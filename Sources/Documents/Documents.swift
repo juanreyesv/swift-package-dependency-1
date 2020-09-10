d20b612ef27e81e87bdcc6447ebd60938d8f52c9
Juan Reyes <juanreyesv@gmail.com>
2020-09-11 08:11:58 +1000

    Making struct public and property static

--- Sources/Documents/Documents.swift
+++ Sources/Documents/Documents.swift
@@ -1,9 +1,11 @@
-struct Documents {
+public struct Documents {
     var text = "Hello, World!"
     
-    var list = [
+    public static let list = [
       "document 1",
       "document 2",
       "document 3"
     ]
+    
+    public init() {}
 }
